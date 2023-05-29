50e416dc6439e04c1559af7160878f3fc1dae2a4
Momowaz <momo.waz1593@gmail.com>
2023-05-28 18:04:06 -0700

    update method to use sql queries

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -8,14 +8,31 @@ const pool = new Pool({
   host: 'localhost',
   database: 'lightbnb'
 });
-// pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.log(response)})
-/// Users
 
 /**
  * Get a single user from the database given their email.
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
+
+const getUserWithEmail = (email) => {
+  return pool
+    .query('SELECT name, email FROM users WHERE users.email = $1', [email])
+    .then((result) => {
+      if (result.rows.length > 0) {
+        const user = result.rows[0];
+        console.log('user is :',user);
+        return user;
+      } else {
+        return null;
+      }
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+/*
 const getUserWithEmail = function (email) {
   let resolvedUser = null;
   for (const userId in users) {
@@ -26,27 +43,64 @@ const getUserWithEmail = function (email) {
   }
   return Promise.resolve(resolvedUser);
 };
+*/
 
 /**
  * Get a single user from the database given their id.
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+// const getUserWithId = function (id) {
+//   return Promise.resolve(users[id]);
+// };
+
+const getUserWithId = (id) => {
+  return pool
+    .query('SELECT name, email FROM users WHERE id = $1', [id])
+    .then((result) => {
+      if (result.rows.length > 0) {
+        const user = result.rows[0];
+        return user;
+      } else {
+        return null;
+      }
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
+
 
 /**
  * Add a new user to the database.
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+// const addUser = function (user) {
+//   const userId = Object.keys(users).length + 1;
+//   user.id = userId;
+//   users[userId] = user;
+//   return Promise.resolve(user);
+// };
+
+const addUser = (user) => {
+  const { name, email, password } = user;
+  const query = 'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *';
+  const values = [name, email, password];
+
+  return pool
+    .query(query, values)
+    .then((result) => {
+      const newUser = result.rows[0];
+
+      console.log('add newUser', newUser);
+      return newUser;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
+
 
 /// Reservations
 
@@ -55,9 +109,34 @@ const addUser = function (user) {
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+// const getAllReservations = function (guest_id, limit = 10) {
+//   return getAllProperties(null, 2);
+// };
+
+const getAllReservations = function(guest_id, limit = 10) {
+  const query = `
+    SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, AVG(rating) AS average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    LEFT JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY reservations.id, properties.id
+    ORDER BY reservations.start_date
+    LIMIT $2;
+  `;
+  const values = [guest_id, limit];
+
+  return pool
+    .query(query, values)
+    .then((result) => {
+      const reservations = result.rows;
+      return reservations;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
+
 
 /// Properties
 
@@ -71,7 +150,6 @@ const getAllProperties = (options, limit = 10) => {
   return pool
     .query(`SELECT * FROM properties LIMIT $1`, [limit])
     .then((result) => {
-      console.log(result.rows);
       return result.rows;
     })
     .catch((err) => {
